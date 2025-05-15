d1c00ee15c30befdf2ffaaceab8291944169b3f8
Daniel Winkler <daniel.winkler@s.wu.ac.at>
2025-05-15 10:39:47 +0200

    Moved to flake templates

--- flake.nix
+++ flake.nix
@@ -1,72 +1,12 @@
 {
-  description = "Project Template";
-  inputs = {
-    nixpkgs = {
-      url = "https://github.com/rstats-on-nix/nixpkgs/archive/2025-05-05.tar.gz";
+  description = "Project Templates";
+  output = {self}: {
+    templates = {
+      r = {
+        path = ./templates/r;
+        description = "R development environment";
+      };
     };
-    nCats.url = "github:dwinkler1/nixCatsConfig";
-    nCats.inputs.nixpkgs.follows = "nixpkgs";
+  defaultTemplate = self.templates.r;
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nCats,
-      ...
-    }@inputs:
-    let
-      forSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
-    in
-    {
-      packages = forSystems (
-        system:
-        let
-
-          inherit (nCats) utils;
-          finalPackage = nCats.packages.${system}.default.override (prev: {
-            categoryDefinitions = utils.mergeCatDefs prev.categoryDefinitions (
-              {
-                pkgs,
-                settings,
-                categories,
-                name,
-                extra,
-                mkPlugin,
-                ...
-              }@packageDef:
-              let
-                rpkgs = import ./rpkgs.nix pkgs;
-              in
-              {
-                lspsAndRuntimeDeps.rdev = with pkgs; [
-                  (rWrapper.override {
-                    packages = rpkgs;
-                  })
-                  just
-                  wget
-                ];
-              }
-            );
-
-            packageDefinitions = prev.packageDefinitions // {
-              nixCats = utils.mergeCatDefs prev.packageDefinitions.nixCats (
-                { ... }:
-                {
-                  settings = {
-                    suffix-path = false;
-                    suffix-LD = false;
-                  };
-                  categories = {
-                    rdev = true;
-                  };
-                }
-              );
-            };
-          });
-        in
-        # and
-        utils.mkAllWithDefault finalPackage
-
-      );
-    };
 }
