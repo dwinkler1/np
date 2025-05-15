23bc9e59425f2d2b7ad02abc5a0528172dc80b2a
Daniel Winkler <daniel.winkler@s.wu.ac.at>
2025-05-15 10:54:13 +0200

    Fix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,13 @@
 {
   description = "Project Templates";
   outputs = {self}: 
-   { ... }:
-    {
+  {
     templates = {
       r = {
         path = ./templates/r;
         description = "R development environment";
       };
     };
-  defaultTemplate = self.templates.r;
+    defaultTemplate = self.templates.r;
   };
 }
