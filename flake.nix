2e8a70ce5fb89ad08fa358d3e97532518e98d04e
Daniel Winkler <dw@dwinkler.org>
2025-08-28 21:21:26 +1000

    Rebrading

--- flake.nix
+++ flake.nix
@@ -2,21 +2,9 @@
   description = "Project Templates";
   outputs = {self}: {
     templates = {
-      n = {
-        path = ./templates/n;
-        description = "Minimal Development environment";
-      };
-      r = {
-        path = ./templates/r;
-        description = "R development environment";
-      };
-      sci = {
-        path = ./templates/sci;
-        description = "Scientific computing environment (Julia, Python, R)";
-      };
-      sci_minimal = {
-        path = ./templates/sci_minimal;
-        description = "Scientific computing environment (Julia, Python, R) without folder structure";
+      rde = {
+        path = ./templates/rde;
+        description = "Research Development Environment";
       };
     };
     defaultTemplate = self.templates.n;
